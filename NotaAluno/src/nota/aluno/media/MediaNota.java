46dac63c3a8d66cccf54db1621110aee861f47ae
Andrew Felipe Moreno <55249944+lipyx@users.noreply.github.com>
2022-05-25 16:59:50 -0300

    Treinamento em Java - calculadora de média

---
+++ NotaAluno/src/nota/aluno/media/MediaNota.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package nota.aluno.media;
+
+/**
+ * @author lipy
+ * Treinamento em Java - calculo de nota média.
+ */
+public class MediaNota {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Calculando a média dos alunos");
+
+	}
+
+}
